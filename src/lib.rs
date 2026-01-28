14024e1a78edf76b7dcb239b3f6b4fff2c703119
Kai Wanke <kai.wanke@web.de>
2026-01-28 21:51:54 +0100

    Include module util. Add unit test section.

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,24 @@
 mod list_items;
+mod utils;
+
+// Section for unit tests
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+    use crate::list_items::structs::*;
+    use crate::utils::functions::*;
+
+    #[test]
+    fn it_sorts_hashmap() {
+        let mut map : HashMap<String, u32> = HashMap::new();
+        map.insert("d".to_string(), 1);
+        map.insert("b".to_string(), 2);
+        map.insert("a".to_string(), 1);
+
+        let vec = sort_list(&map);
+
+        assert_eq!(vec[0].0, "a");
+        assert_eq!(vec[1].0, "b");
+        assert_eq!(vec[2].0, "d");
+    }
+}
