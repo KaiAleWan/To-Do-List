6944f8348697acb9d91f3203c059ee22c3e83534
Kai Wanke <kai.wanke@web.de>
2026-01-30 18:15:53 +0100

    Add comments.

--- src/list_items/enums.rs
+++ src/list_items/enums.rs
@@ -1,11 +1,26 @@
+
+/// The `Priority` enum is used to store the priority assigned to an Item in the ToDoList.
+#[derive(Debug, Clone)]
 pub enum Priority {
+    /// Indicates low priority task
     Low,
+    /// Indicates medium priority task
     Medium,
+    /// Indicates high priority task
     High,
+    /// Is assigned when an invalid value was submitted to initialize the Priority struct
     Invalid,
 }
 
 impl Priority {
+    /// Derives a new Priority instance from a &str input value. 
+    /// Permissable values are "low", "medium", or "high". For any other value, the `invalid` Priority is assigned.
+    /// 
+    /// # Arguments
+    /// * item_name : &str - Desired Priority variant
+    /// 
+    /// # Returns
+    /// * `Priority`: A new Priority instance
     pub fn from_str(input: &str) -> Self {
         if input.to_lowercase().eq("low") {
             Self::Low
@@ -22,6 +37,7 @@ impl Priority {
 use std::error::Error;
 use std::fmt::{Display, Formatter, Result};
 
+/// Enum to handle errors caused by the invalid selection of ToDOList Items.
 #[derive(Debug)]
 #[non_exhaustive]
 pub enum ToDoSelectionError {
